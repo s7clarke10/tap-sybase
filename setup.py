7bc6282e9f4fafe7792049aa313e200e9a590994
Steve Clarke <84364906+s7clarke10@users.noreply.github.com>
2023-10-24 15:02:52 +1300

    Pinning pymssql to version less than 2.2.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "pendulum>=1.2.0",
         "singer-python==5.13.0",
 #        pymssql==2.2.8 broken: https://github.com/pymssql/pymssql/issues/833
-        "pymssql>=2.1.1,!=2.2.8",
+        "pymssql>=2.1.1,<=2.2.7",
         "backoff==1.8.0",
     ],
     entry_points="""
