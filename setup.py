40953cc11965dc98630906b974694f5608bac0f8
Steve Clarke <84364906+s7clarke10@users.noreply.github.com>
2024-12-01 21:27:14 +1300

    Support older versions of Sybase via TDS version 4.2 (#25)
    * Bumping pymssql version to examine driver issue
    
    * Trying TDS 4.2 if there is a connection error. This feature is for older versions of Sybase.
    
    * Setting TDS version as a string.
    
    * Bumping Version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="tap-sybase",
-    version="1.0.13",
+    version="1.1.0",
     description="Singer.io tap for extracting data from SQL Server - PipelineWise compatible",
     author="Stitch",
     url="https://github.com/s7clarke10/tap-sybase",
@@ -17,8 +17,7 @@ setup(
         "attrs>=24.2.0",
         "pendulum>=1.2.0",
         "realit-singer-python>=5.0.0",
-#        pymssql==2.2.8 broken: https://github.com/pymssql/pymssql/issues/833
-        "pymssql>=2.1.4,!=2.2.8",
+        "pymssql>=2.2.9",
         "backoff>=1.8.0",
     ],
     entry_points="""
